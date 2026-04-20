5c19194afef45d9c272e92ebe68625b82c4808c3
tinhdaysauconmeanhchinhohaitayhaicumombucuemtrongconmeroianhnhinlenbautroigiua1nganvisaoanhchithayminhemtrongbongdemdangmoccheovoinyc <97789236+chemchetchagio@users.noreply.github.com>
2026-04-20 23:32:45 +0700

    added action because im lazy

---
+++ sh.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+COMMIT="$(git rev-parse --short HEAD)"
+
+echo "Creating module (*.zip)..."
+cd module_new/tensor-canary
+zip -r Tensor-VoLTE-VoWifi-Canary-$COMMIT.zip . >/dev/null 2>&1
+cd ..
+cd ..
+mv module_new/tensor-canary/Tensor-VoLTE-VoWifi-Canary-*.zip Tensor-VoLTE-VoWifi-Canary-$COMMIT.zip
+
+if [ -f Tensor-VoLTE-VoWifi-Canary-*.zip ]; then
+	echo "Module saved to [Tensor-VoLTE-VoWifi-Canary-*.zip], try to eat it "
+	rm -rf module_new/tensor-canary/*.zip
+else
+	echo "discordmess mat roi [Tensor-VoLTE-VoWifi-Canary-*.zip] ?"
+fi
